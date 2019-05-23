320a06511e0a678fcc9b50125bf2193137138747
Gonçalo Graça Ribeiro <gogasman@gmail.com>
2019-05-23 22:17:44 +0100

    conversão das queries raw para migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,10 +17,16 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('name')->unique();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            //$table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+
+            $table->text('google_code')->nullable()->default(null);
+            $table->text('fido_code')->nullable()->default(null);
+            $table->text('sms_code')->nullable()->default(null);
+            $table->text('email_code')->nullable()->default(null);
+
         });
     }
 
---
+++ database/migrations/2019_05_23_205326_create_login_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLoginTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logins', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('source')->default('Website');
+            $table->string('ip');
+            $table->timestamps();
+
+        });
+
+        Schema::table('logins', function($table) {
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('logins');
+    }
+}
