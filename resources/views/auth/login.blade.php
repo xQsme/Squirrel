aba82730a7b0b8c623c2ce1219eb91b66c60bf62
xQsme <2151163@my.ipleiria.pt>
2019-05-27 01:51:38 +0100

    button

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -48,7 +48,7 @@
                                 </button>
 
                                 @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    <a class="btn btn-warning" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
                                     </a>
                                 @endif
