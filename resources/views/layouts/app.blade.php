c5d729e1600ea80da4e42e60428e993cc35814cc
xQsme <2151163@my.ipleiria.pt>
2019-05-28 12:19:32 +0100

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -68,6 +68,8 @@
     </nav>
     @if(isset($banner))
         <img id="banner" src="./img/banner.jpg" class="banner">
+        <img style="display: none" src="./img/banner-sec.jpg" class="banner">
+        <img style="display: none" src="./img/banner-slim.jpg" class="banner">
     @endif
     <main class="py-4">
         @yield('content')
